7d6144cba78849bd9f625c590df9d17a3bbe77a2
facumarcati <facumarcati14@gmail.com>
2023-05-10 18:02:53 -0300

    Clase pelicula y categorias - settters, getters, toString

--- TP5_Grupo6/src/aplicacion_escritorio/Categoria.java
+++ TP5_Grupo6/src/aplicacion_escritorio/Categoria.java
@@ -2,4 +2,34 @@ package aplicacion_escritorio;
 
 public class Categoria {
 
+	private int ID;
+	private String Nombre;
+	
+	public Categoria() {
+		
+	}
+	
+	public Categoria(int iD, String nombre) {
+		super();
+		ID = iD;
+		Nombre = nombre;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	
+	@Override
+	public String toString() {
+		return Nombre;
+	}
+	
 }
--- TP5_Grupo6/src/aplicacion_escritorio/Pelicula.java
+++ TP5_Grupo6/src/aplicacion_escritorio/Pelicula.java
@@ -2,4 +2,41 @@ package aplicacion_escritorio;
 
 public class Pelicula {
 
+	private Categoria categoria;
+	private int ID;
+	private String nombre;	
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Pelicula() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return ID + " - " + nombre + " - " + categoria.getNombre();
+	}
+	
 }
